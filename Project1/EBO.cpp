4fc8120354f6a12c6428635030ca60370838e658
me <me@LAPTOP>
2018-10-04 22:00:13 +0100

    created template for EBO class

---
+++ Project1/EBO.cpp
@@ -0,0 +1,21 @@
+#include "EBO.h"
+
+ElementBufferObject::ElementBufferObject(GLint * ibo)
+{
+}
+
+ElementBufferObject::~ElementBufferObject()
+{
+}
+
+void ElementBufferObject::Bind(void)
+{
+}
+
+void ElementBufferObject::BufferData(GLenum drawtype)
+{
+}
+
+void ElementBufferObject::Unbind(void)
+{
+}
---
+++ Project1/EBO.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef GLEW_STATIC
+#define GLEW_STATIC
+#endif 
+#include<GL\glew.h>
+#include<GLFW\glfw3.h>
+
+class ElementBufferObject
+{
+private:
+	GLuint handle;
+	GLint* ibo;
+public:
+	ElementBufferObject(GLint* ibo);
+	~ElementBufferObject();
+	void Bind(void);
+	void BufferData(GLenum drawtype);
+	void Unbind(void);
+};
