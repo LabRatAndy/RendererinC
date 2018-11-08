f7ed1c423bed245b46357c0cef51aef3080be6fb
me <me@LAPTOP>
2018-11-08 22:00:50 +0000

    Change to use only map/dictionary and therefore can only get and remove with a name

--- Project1/ResourceManager.cpp
+++ Project1/ResourceManager.cpp
@@ -12,3 +12,42 @@ ResourceManager & ResourceManager::Instance(void)
 	static ResourceManager instance;
 	return instance;
 }
+
+void ResourceManager::AddShader(Shader * shader, GLchar * shadername)
+{
+}
+
+Shader * ResourceManager::GetShader(GLchar * shadername)
+{
+	return nullptr;
+}
+
+void ResourceManager::RemoveShader(GLchar * shadername)
+{
+}
+
+void ResourceManager::AddCamera(Camera * camera, GLchar * cameraname)
+{
+}
+
+Camera * ResourceManager::GetCamera(GLchar * cameraname)
+{
+	return nullptr;
+}
+
+void ResourceManager::RemoveCamera(GLchar * cameraname)
+{
+}
+
+void ResourceManager::AddTexture(Texture * texture, GLchar * texturename)
+{
+}
+
+Texture * ResourceManager::GetTexture(GLchar * texturename)
+{
+	return nullptr;
+}
+
+void ResourceManager::RemoveTexture(GLchar * texturename)
+{
+}
--- Project1/ResourceManager.h
+++ Project1/ResourceManager.h
@@ -16,30 +16,22 @@ class ResourceManager
 private:
 	ResourceManager();
 	~ResourceManager();
-	std::vector<Shader*> shaderList;
-	std::vector<Texture*> textureList;
-	std::vector<Camera*> cameraList;
-	std::map<GLchar*,GLint> shaderDictionary;
-	std::map<GLchar*,GLint> textureDictionary;
-	std::map<GLchar*,GLint> cameraDictionary;
+	std::map<GLchar*,Shader*> shaderDictionary;
+	std::map<GLchar*,Texture*> textureDictionary;
+	std::map<GLchar*,Camera*> cameraDictionary;
 	
 public:
 	static ResourceManager& Instance(void);
-	GLint AddShader(Shader* shader, GLchar* shadername);
-	Shader* GetShader(GLint index);
+	void AddShader(Shader* shader, GLchar* shadername);
 	Shader* GetShader(GLchar* shadername);
-	void RemoveShader(GLint index);
 	void RemoveShader(GLchar* shadername);
-	GLint AddCamera(Camera* camera, GLchar* cameraname);
-	Camera* GetCamera(GLint index);
+	void AddCamera(Camera* camera, GLchar* cameraname);
 	Camera* GetCamera(GLchar* cameraname);
-	void RemoveCamera(GLint index);
 	void RemoveCamera(GLchar* cameraname);
-	GLint AddTexture(Texture* texture, GLchar* texturename);
-	Texture* GetTexture(GLint index);
+	void AddTexture(Texture* texture, GLchar* texturename);
 	Texture* GetTexture(GLchar* texturename);
-	void RemoveTexture(GLint index);
 	void RemoveTexture(GLchar* texturename);
+
 	//copy constructor and assignment operator diablement
 	ResourceManager(const ResourceManager&) = delete;
 	ResourceManager(ResourceManager&&) = delete;
